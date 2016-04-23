3a58b008e003f0ad933c1eb45f082ff48ebb9830
jdavin <jdavin@student.42.fr>
2016-04-23 20:31:54 +0200

    degrade rouge-vert-bleu

--- ft_test.c
+++ ft_test.c
@@ -6,7 +6,7 @@
 /*   By: jdavin <jdavin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/23 14:20:32 by jdavin            #+#    #+#             */
-/*   Updated: 2016/04/23 17:07:56 by jdavin           ###   ########.fr       */
+/*   Updated: 2016/04/23 20:30:45 by jdavin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,77 @@ void		ft_test(t_data *e)
 	int				octet;
 	int				pos;
 
-	color.rgb[0] = 255;
-	color.rgb[1] = 0;
-	color.rgb[2] = 0;
-	color.rgb[3] = 0;
+
 	octet = e->sizeline / WDH;
-	pos = 240 * e->sizeline;
-	while (pos < 481 * e->sizeline)
+	pos = 0;
+
+	color.rgb[0] = 225;
+	color.rgb[1] = 225;
+	color.rgb[2] = 225;
+	color.rgb[3] = 0;
+	while (pos < 45 * e->sizeline)
 	{
 		if (pos % e->sizeline == 0)
 		{
-			color.rgb[2]++;
-			color.rgb[1]++;
+			color.rgb[0] -= 5;
+			color.rgb[1] -= 5;
+			color.rgb[2] -= 5;
+		}
+		if (ft_memcmp(e->data + pos, &(color.color), octet) != 0)
+			ft_memcpy(e->data + pos, &(color.color), octet);
+		pos += octet;
+	}
+	color.rgb[2] = 45;
+	while (pos < 180 * e->sizeline)
+	{
+		if (pos % e->sizeline == 0)
+		{
+			color.rgb[2] += 1;
+		}
+		if (ft_memcmp(e->data + pos, &(color.color), octet) != 0)
+			ft_memcpy(e->data + pos, &(color.color), octet);
+		pos += octet;
+	}
+	while (pos < 360 * e->sizeline)
+	{
+		if (pos % e->sizeline == 0)
+		{
+			color.rgb[2]--;
+			color.rgb[1]++;	
+		}
+		if (ft_memcmp(e->data + pos, &(color.color), octet) != 0)
+			ft_memcpy(e->data + pos, &(color.color), octet);
+		pos += octet;
+	}
+	while (pos < 540 * e->sizeline)
+	{
+		if (pos % e->sizeline == 0)
+		{
+			color.rgb[1]--;
+			color.rgb[0]++;
+		}
+		if (ft_memcmp(e->data + pos, &(color.color), octet) != 0)
+			ft_memcpy(e->data + pos, &(color.color), octet);
+		pos += octet;
+	}
+	while (pos < 675 * e->sizeline)
+	{
+		if (pos % e->sizeline == 0)
+		{
+			color.rgb[0] -= 1;
+		}
+		if (ft_memcmp(e->data + pos, &(color.color), octet) != 0)
+			ft_memcpy(e->data + pos, &(color.color), octet);
+		pos += octet;
+	}
+	color.rgb[0] = 0;
+	while (pos < 720 * e->sizeline)
+	{
+		if (pos % e->sizeline == 0)
+		{
+			color.rgb[0] += 5;
+			color.rgb[1] += 5;
+			color.rgb[2] += 5;
 		}
 		if (ft_memcmp(e->data + pos, &(color.color), octet) != 0)
 			ft_memcpy(e->data + pos, &(color.color), octet);
