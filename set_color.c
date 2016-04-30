abb61d661cba8b593228d5195c4edb9bca7b1c82
jdavin <jdavin@student.42.fr>
2016-04-30 03:05:26 +0200

    couleurs

--- set_color.c
+++ set_color.c
@@ -6,7 +6,7 @@
 /*   By: jdavin <jdavin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/27 21:39:52 by jdavin            #+#    #+#             */
-/*   Updated: 2016/04/30 02:54:35 by jdavin           ###   ########.fr       */
+/*   Updated: 2016/04/30 03:04:45 by jdavin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 static void		setrgb(t_color *color, char r, char g, char b)
 {
-	color->rgb[0] = r;
+	color->rgb[0] = b;
 	color->rgb[1] = g;
-	color->rgb[2] = b;
+	color->rgb[2] = r;
 	color->rgb[3] = 0;
 }
 
@@ -29,17 +29,17 @@ void			set_i_color(int i, t_data *e)
 
 	l = 0.5 * (i < e->maxiter);
 	a = 1 - fabs(2 * l - 1);
-	b = a * (1 - fabs(fmod((i / 51.0), 2) - 1));
+	b = a * (1 - fabs(fmod((i / 21.0), 2) - 1));
 	c = l - 0.5 * a;
-	if (i >= 0 && i < 51)
+	if (i >= 0 && i < 21)
 		setrgb(&e->color, (a + c) * 255, (b + c) * 255, (0 + c) * 255);
-	else if (i >= 51 && i < 102)
+	else if (i >= 21 && i < 42)
 		setrgb(&e->color, (b + c) * 255, (a + c) * 255, (0 + c) * 255);
-	else if (i >= 102 && i < 153)
+	else if (i >= 42 && i < 102)
 		setrgb(&e->color, (0 + c) * 255, (a + c) * 255, (b + c) * 255);
-	else if (i >= 153 && i < 204)
+	else if (i >= 102 && i < 204)
 		setrgb(&e->color, (0 + c) * 255, (b + c) * 255, (a + c) * 255);
-	else if (i >= 204 && i < 400)
+	else if (i >= 200 && i < 400)
 		setrgb(&e->color, (b + c) * 255, (0 + c) * 255, (a + c) * 255);
 	else
 		setrgb(&e->color, 255, 255, 255);
