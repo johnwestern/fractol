d40575b153afb50cd3c4413e8b46e411b03407b7
jdavin <jdavin@student.42.fr>
2016-05-05 03:24:12 +0200

    amelioration du black&white

--- set_color2.c
+++ set_color2.c
@@ -6,7 +6,7 @@
 /*   By: jdavin <jdavin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/03 15:25:13 by jdavin            #+#    #+#             */
-/*   Updated: 2016/05/03 17:42:47 by jdavin           ###   ########.fr       */
+/*   Updated: 2016/05/05 03:23:29 by jdavin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,10 @@ static void		enhanced_start(int i, t_data *e, t_flcl *f)
 	f->l = 0.5 * (i < e->mitr);
 	f->a = 1 - fabs(2 * f->l - 1);
 	f->b = f->a * (1.0 - fabs(fmod((i / 50.0), 2) - 1.0));
-	f->c = f->l - 0.5 * f->a;
-	setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
+	if (i >= 0 && i < 50)
+		setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
+	else if (i >= 50 && i < 100)
+		setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
 }
 
 static void		enhanced_end(int i, t_data *e, t_flcl *f)
@@ -34,39 +36,33 @@ static void		enhanced_end(int i, t_data *e, t_flcl *f)
 	f->l = 0.5 * (i < (e->mitr - 250));
 	f->a = 1 - fabs(2 * f->l - 1);
 	f->b = f->a * (1.0 - fabs(fmod((i / 50.0), 2) - 1.0));
-	f->c = f->l - 0.5 * f->a;
 	if (i >= 50 && i < 100)
-		setrgb(&e->color, (f->a + f->c) * 255, (f->a + f->c) * 255, \
-			(f->a + f->c) * 255);
+		setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
 	else if (i >= 100 && i < 150)
-		setrgb(&e->color, 0, f->b * 255, f->c * 255);
+		setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
 	else if (i >= 150 && i < 200)
-		setrgb(&e->color, f->c * 255, f->b * 255, f->a * 255);
+		setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
 	else if (i >= 200 && i < 250)
-		setrgb(&e->color, f->b * 255, f->c * 255, f->c * 255);
+		setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
 	else if (i >= 250 && i < 300)
-		setrgb(&e->color, f->b * 255, f->c * 255, f->c * 255);
+		setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
 	else if (i >= 250 && i < 350)
-		setrgb(&e->color, f->a * 255, f->a * 255, f->a * 255);
+		setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
 }
 
 void			set_bw_color(int i, t_data *e)
 {
 	auto t_flcl		f;
 
-	if (i >= 0 && i < 50)
+	if (i >= 0 && i < 100)
 		enhanced_start(i, e, &f);
-	if (i >= 300 && i < 600)
+	else if (i >= 300 && i < 600)
 		enhanced_end(i - 250, e, &f);
 	f.l = 0.5 * (i < e->mitr);
 	f.a = 1 - fabs(2 * f.l - 1);
 	f.b = f.a * (1.0 - fabs(fmod((i / 100.0), 2) - 1.0));
-	f.c = f.l - 0.5 * f.a;
-	if (i >= 50 && i < 100)
-		setrgb(&e->color, (f.a + f.c) * 255, (f.a + f.c) * 255, f.a * 255);
-	else if (i >= 100 && i < 200)
-		setrgb(&e->color, (f.b + f.c) * 255, (f.b + f.c) * 255, f.b * 255);
+	if (i >= 100 && i < 200)
+		setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
 	else if (i >= 200 && i < 300)
-		setrgb(&e->color, (f.a + f.c) * 255, (f.a + f.c) * 255, \
-			(f.a + f.c) * 255);
+		setrgb(&e->color, f->b * 255, f->b * 255, f->b * 255);
 }
